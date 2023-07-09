7e80ab21dcdc02a2226b9a2341d27ba23b6020df
Johannes Huwald <johannes.huwald@gmail.com>
2023-07-09 17:15:37 +0200

    Implement simple ical querying using minicaldev

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,23 @@ pub mod server;
 
 use std::path::Path;
 
-fn main() -> Result<(), server::ConfigError> {
-    let config = server::Config::from_toml(Path::new("config.toml"))?;
-    println!("Config: {:?}", config);
-
+fn main() -> Result<(), server::Error> {
+    let server = server::Server::from_toml(Path::new("config.toml"))?;
+    for task in server.query_tasks()? {
+        println!(
+            "Name: {} ({})",
+            task.ical().name,
+            task.etag().unwrap_or(&String::from(""))
+        );
+        println!("Properties:");
+        for prop in task.ical().properties.iter() {
+            println!("\t{:?}", prop);
+        }
+        println!("Children:");
+        for child in task.ical().children.iter() {
+            println!("\t{:?}", child);
+        }
+        println!()
+    }
     Ok(())
 }
--- src/server/mod.rs
+++ src/server/mod.rs
@@ -1,11 +1,13 @@
+use minicaldav::{Calendar, Credentials, Error as CaldavError};
 use std::fs;
 use std::io::Error as IOError;
 use std::path::Path;
 use toml;
+use ureq::Agent;
 use url::{ParseError as UrlError, Url};
 
 #[derive(Debug)]
-pub enum ConfigError {
+pub enum Error {
     File(IOError),
     Url(UrlError),
     Parsing(toml::de::Error),
@@ -14,62 +16,101 @@ pub enum ConfigError {
         field: &'static str,
         section: &'static str,
     },
-    InvalidSyntax,
+    Caldav(CaldavError),
+    InvalidConfigValue(&'static str),
 }
 
-impl From<IOError> for ConfigError {
+impl From<IOError> for Error {
     fn from(err: IOError) -> Self {
         Self::File(err)
     }
 }
 
-impl From<UrlError> for ConfigError {
+impl From<UrlError> for Error {
     fn from(err: UrlError) -> Self {
         Self::Url(err)
     }
 }
-impl From<toml::de::Error> for ConfigError {
+
+impl From<toml::de::Error> for Error {
     fn from(err: toml::de::Error) -> Self {
         Self::Parsing(err)
     }
 }
 
-#[derive(Debug)]
-pub struct Config {
-    pub url: Url,
-    pub user: String,
-    pub password: String,
+impl From<minicaldav::Error> for Error {
+    fn from(err: minicaldav::Error) -> Self {
+        Self::Caldav(err)
+    }
+}
+
+struct Config {
+    url: Url,
+    credentials: Credentials,
+    calendar_name: String,
+}
+
+pub struct Server {
+    config: Config,
+    calendar: Calendar,
 }
 
 impl Config {
-    pub fn from_toml(file: &Path) -> Result<Self, ConfigError> {
-        let table = fs::read_to_string(file)?.parse::<toml::Table>()?;
-        let server = table
-            .get("Server")
-            .ok_or(ConfigError::MissingSection("Server"))?;
-
-        if let toml::Value::Table(server) = server {
-            fn get_string<'a>(
-                table: &'a toml::Table,
-                field: &'static str,
-            ) -> Result<&'a str, ConfigError> {
-                let field = table.get(field).ok_or(ConfigError::MissingField {
-                    field,
-                    section: "Server",
-                })?;
-                match field {
-                    toml::Value::String(s) => Ok(s),
-                    _ => Err(ConfigError::InvalidSyntax),
-                }
+    pub fn from_toml(file: &Path) -> Result<Self, Error> {
+        fn get_section<'a>(
+            table: &'a toml::Table,
+            section: &'static str,
+        ) -> Result<&'a toml::Table, Error> {
+            match table.get(section).ok_or(Error::MissingSection(section))? {
+                toml::Value::Table(section) => Ok(section),
+                _ => Err(Error::InvalidConfigValue("Invalid section")),
             }
-            return Ok(Config {
-                url: Url::parse(get_string(&server, "url")?)?,
-                user: get_string(&server, "user")?.to_owned(),
-                password: get_string(&server, "password")?.to_owned(),
-            });
         }
 
-        // TODO: Improve usefulness of this error
-        Err(ConfigError::InvalidSyntax)
+        fn get_string<'a>(
+            table: &'a toml::Table,
+            field: &'static str,
+            section: &'static str,
+        ) -> Result<&'a str, Error> {
+            let field = table
+                .get(field)
+                .ok_or(Error::MissingField { field, section })?;
+            match field {
+                toml::Value::String(s) => Ok(s),
+                _ => Err(Error::InvalidConfigValue("Invalid field")),
+            }
+        }
+
+        let table = fs::read_to_string(file)?.parse::<toml::Table>()?;
+        let server = get_section(&table, "Server")?;
+        let calendar = get_section(&table, "Calendar")?;
+
+        let user = get_string(&server, "user", "Server")?.to_owned();
+        let password = get_string(&server, "password", "Server")?.to_owned();
+        Ok(Config {
+            url: Url::parse(get_string(&server, "url", "Server")?)?,
+            credentials: minicaldav::Credentials::Basic(user, password),
+            calendar_name: get_string(&calendar, "name", "Calendar")?.to_owned(),
+        })
+    }
+}
+
+impl Server {
+    pub fn from_toml(file: &Path) -> Result<Self, Error> {
+        let config = Config::from_toml(file)?;
+        let calendar = minicaldav::get_calendars(Agent::new(), &config.credentials, &config.url)?
+            .iter()
+            .find(|c| c.name() == &config.calendar_name)
+            .ok_or(Error::InvalidConfigValue("Invalid calendar name"))?
+            .to_owned();
+
+        Ok(Self { config, calendar })
+    }
+
+    pub fn query_tasks(&self) -> Result<Vec<minicaldav::Event>, Error> {
+        // TODO: What to do with errors?
+        let (events, _errors) =
+            minicaldav::get_todos(Agent::new(), &self.config.credentials, &self.calendar)?;
+        Ok(events)
     }
 }
